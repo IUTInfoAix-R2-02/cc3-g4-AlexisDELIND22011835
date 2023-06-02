803de18bd83210727ebde8c76be3c75af064444e
d22011835 <alexis.delin@etu.univ-amu.fr>
2023-06-02 14:58:37 +0200

    partie 1 fait de maniere à pouvoir passer rapidement à la suite partie 2 commencé sans que ça marche

--- src/main/java/fr/amu/iut/cc3/ToileApp.java
+++ src/main/java/fr/amu/iut/cc3/ToileApp.java
@@ -1,12 +1,15 @@
 package fr.amu.iut.cc3;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
 
 public class ToileApp extends Application {
 
--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -1,27 +1,15 @@
 package fr.amu.iut.cc3;
 
-import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class ToileController implements Initializable {
@@ -31,10 +19,61 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
+    @FXML
+    public Button tracer;
+
+    @FXML
+    public Button vider;
+
+    @FXML
+    TextField comp1;
+
+    @FXML
+    TextField comp2;
+
+    @FXML
+    TextField comp3;
+
+    @FXML
+    TextField comp4;
+
+    @FXML
+    TextField comp5;
+
+    @FXML
+    TextField comp6;
+
+    @FXML
+    Pane circle;
+
+    public Double[] valueList;
+
+    //DoubleProperty note = new SimpleDoubleProperty();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        tracer.setOnAction(e -> tracerValeur());
     }
+
+    void tracerValeur(){
+        valueList [0] = Double.valueOf(comp1.getText());
+        valueList [1] = Double.valueOf(comp2.getText());
+        valueList [2] = Double.valueOf(comp3.getText());
+        valueList [3] = Double.valueOf(comp4.getText());
+        valueList [4] = Double.valueOf(comp5.getText());
+        valueList [5] = Double.valueOf(comp6.getText());
+
+        for(int index =0;index <6; index++){
+            int xValue = getXRadarChart(valueList[index],index+1);
+            int yValue = getYRadarChart(valueList[index],index+1);
+            Circle pointNote = new Circle();
+            pointNote.setCenterX(xValue);
+            pointNote.setCenterY(yValue);
+            circle.getChildren().add(pointNote);
+        }
+    }
+
 
     int getXRadarChart(double value, int axe ){
         return (int) (rayonCercleExterieur + Math.cos(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
